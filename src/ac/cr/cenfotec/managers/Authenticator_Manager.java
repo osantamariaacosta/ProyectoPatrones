01f8a48fb67766573a087b330ea93a677fc08687
osantamariaacosta <osantamariaa@ucenfotec.ac.cr>
2018-06-28 21:36:21 -0600

    Manager Autenticador
    metodo inicial

--- src/ac/cr/cenfotec/managers/Authenticator_Manager.java
+++ src/ac/cr/cenfotec/managers/Authenticator_Manager.java
@@ -2,7 +2,17 @@
  * 
  */
 package ac.cr.cenfotec.managers;
+import ac.cr.cenfotec.clases.Authenticator;
+import ac.cr.cenfotec.clases.UserAuthenticated;;
+
 
 public class Authenticator_Manager {
-
+	private Authenticator authenticator = new Authenticator();
+	private UserAuthenticated userAuthenticated = new UserAuthenticated();
+	
+	public boolean validateCredentials (String userName, String password) throws Exception {
+		boolean isAuthenticated = false;
+		isAuthenticated = authenticator.validateCredencials(userName, password);
+		return false;
+	}
 }
