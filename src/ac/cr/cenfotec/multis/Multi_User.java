0c3cd53c2b5e200cef04aedc83e4a00d282aadc9
osantamariaacosta <osantamariaa@ucenfotec.ac.cr>
2018-06-29 22:53:54 -0600

    login funcional
    Login funcional con datos emulados
    logica para UI

--- src/ac/cr/cenfotec/multis/Multi_User.java
+++ src/ac/cr/cenfotec/multis/Multi_User.java
@@ -2,13 +2,38 @@
  * 
  */
 package ac.cr.cenfotec.multis;
+import java.util.ArrayList;
+
 import ac.cr.cenfotec.clases.User;
 
 public class Multi_User {
 
 	
-	public User getUserByUserName (String userName) 
-	{
-		return null;
+	public ArrayList<User> getUsers () {
+		ArrayList<User> userList = new ArrayList<User>();
+		User newUser = new User("Eduardo","Martinez", 01, "abc123", "eduMar", 0);
+		User newEmployee = new User("Carlos","Garro", 02, "abc123", "carGa", 1);
+		userList.add(newUser);
+		userList.add(newEmployee);
+		return userList;
+	}	
+	
+	public User getUserByUserName (String userName) {
+		User foundedUser = new User();
+		ArrayList<User> userList  = new ArrayList<>();
+		userList = getUsers();
+		
+		for (int i = 0; i < userList.size(); i++) {
+			String testUserName = userList.get(i).getUserName();
+			if (testUserName.equals(userName)) {
+				foundedUser = userList.get(i);
+				break;
+			} else {
+				foundedUser = null;
+			}
+		} 
+		return foundedUser;
 	}
+	
+	
 }
