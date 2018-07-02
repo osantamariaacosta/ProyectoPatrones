69b9bfbecf31d0ff5ee59ea2d448cda9ea02679e
Javier Pozuelo <jpozuelom@ucenfotec.ac.cr>
2018-07-01 19:29:22 -0600

    estructura

--- src/ac/cr/cenfotec/gestores/GestorUser.java
+++ src/ac/cr/cenfotec/gestores/GestorUser.java
@@ -3,22 +3,20 @@ package ac.cr.cenfotec.gestores;
 import ac.cr.cenfotec.multis.*;
 import java.util.ArrayList;
 import ac.cr.cenfotec.clases.Employee;
+import java.sql.SQLException;
 
 public class GestorUser {
+	Multi_User multi = new Multi_User();
 
 	public void registerEmployee(String name, String lastName, int id, String password, String userName, int userType) throws java.lang.ClassNotFoundException, java.sql.SQLException, Exception{
 		
-		 	Multi_User multi;
-	        multi = new Multi_User();
+		 	
 	        multi.registerEmployee(name, lastName, id, password, userName, userType);
 	}
 	
 	  public ArrayList<Employee> listarEmpleado() throws java.lang.ClassNotFoundException, java.sql.SQLException, Exception {
 
 	        ArrayList<Employee> lista;
-
-	        Multi_User multi;
-	        multi = new Multi_User();
 	        lista = multi.listarEmpleado();
 
 	        return lista;
